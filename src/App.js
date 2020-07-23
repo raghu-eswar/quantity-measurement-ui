28fcd066607091f8bc0d671171c2c85f934e615d
raghueswar55 <raghueswar55@gmail.com>
2020-07-23 12:46:55 +0530

    Implemented header components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header.jsx';
+import appData from './commons/data.json';
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Header name={appData.compamyName} navLinks={appData.navigation}></Header>
+    </Router> 
   );
 }
 

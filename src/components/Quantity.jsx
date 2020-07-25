b75aedc155523e5d4687858a94b64addcd485453
raghueswar55 <raghueswar55@gmail.com>
2020-07-25 13:01:50 +0530

    Refactored quantity component to maintain dry principle.

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -3,23 +3,10 @@ import '../styles/quantity.css'
 
 class Quantity extends Component {
 
-    componentDidMount() {
-        if(this.props.isActive) {
-            this.refs[`${this.props.quantity.name}`].classList.add(`${this.props.quantity.name}-active`);
-        }
-    }
-
-    componentDidUpdate() {
-        if(this.props.isActive) {
-            this.refs[`${this.props.quantity.name}`].classList.add(`${this.props.quantity.name}-active`);
-        } else {
-            this.refs[`${this.props.quantity.name}`].classList.remove(`${this.props.quantity.name}-active`);
-        }
-    }
-
     render() {
         return ( 
-            <button ref={this.props.quantity.name} className="quantity" id={this.props.quantity.name} onClick={this.props.onlick.bind(this, this.props.quantity)}>
+            <button ref={this.props.quantity.name} className={this.props.isActive ? `quantity ${this.props.quantity.name}-active` : `quantity`} 
+                    id={this.props.quantity.name} onClick={this.props.onlick.bind(this, this.props.quantity)}>
                 <div></div>
                 <p>{this.props.quantity.name}</p>
             </button>

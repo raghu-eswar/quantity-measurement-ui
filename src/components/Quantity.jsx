4eac1bd35b2400ca6aac98c8b2f5133f6d93b6ac
raghueswar55 <raghueswar55@gmail.com>
2020-07-25 13:21:47 +0530

    Added conditional re rendering for quantity.

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -3,6 +3,10 @@ import '../styles/quantity.css'
 
 class Quantity extends Component {
 
+    shouldComponentUpdate(nextProps) {
+        return (this.props.isActive !== nextProps.isActive)
+    }
+
     render() {
         return ( 
             <button ref={this.props.quantity.name} className={this.props.isActive ? `quantity ${this.props.quantity.name}-active` : `quantity`} 

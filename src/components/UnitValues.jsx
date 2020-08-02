a56c4660ffd1a61ab333375ec4da965d25523ace
raghueswar55 <raghueswar55@gmail.com>
2020-08-02 20:36:47 +0530

    Handled enter in input field to change focus.

--- src/components/UnitValues.jsx
+++ src/components/UnitValues.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import * as Styled from '../styles/unitValues.styled';
 
 function UnitValues(props) {
+
+    const changeFocus = (event) => {
+        if(event.keyCode === 13) {
+            let nextField = (props.unitType === 'from')? props.unitValueRef.to : props.unitValueRef.from;
+            nextField.current.focus()
+        }
+    }
+
     return ( 
         <Styled.UnitContainer id={props.unitType}>
             <Styled.Title>
                 {props.unitType}
             </Styled.Title>
-            <Styled.UnitValue ref={props.unitValueRef[props.unitType]} onChange={(event) => props.onValueChange(event)} value={props.value} type="number"/>
+            <Styled.UnitValue ref={props.unitValueRef[props.unitType]} autoFocus={props.unitType === 'from'} onKeyUp={changeFocus} onChange={(event) => props.onValueChange(event)} value={props.value} type="number"/>
             <Styled.UnitTypes ref={props.unitOptionsRef[props.unitType]} onChange={(event)=>props.onUnitChange(event)}>
                 {props.units.map(unit=> {
                     if(unit !== props.fromUnit)

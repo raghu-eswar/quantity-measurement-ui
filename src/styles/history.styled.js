3a97f0c709b8028e9ffaad8326684725355a1c94
raghueswar55 <raghueswar55@gmail.com>
2020-08-04 18:48:55 +0530

    Modified history page to made responsive.

--- src/styles/history.styled.js
+++ src/styles/history.styled.js
@@ -9,10 +9,13 @@ export const History = styled.div `
     align-items: center;
     justify-content: flex-start;
     overflow: auto;
+    @media ${devices.mobileS} {
+        padding: 0 2% 0 2%       
+    }
 `
 
 export const Content = styled.div `
-    width: 50%;
+    width: 60%;
     height: 92%;
     margin-top: 2%;
     display: flex;
@@ -20,17 +23,35 @@ export const Content = styled.div `
     @media ${devices.iPad} {
         width: 90%;
     }
+    @media ${devices.mobileS} {
+        width: 100%;
+    }
 `
 
 export const Title = styled.h2 `
     margin: 2% auto 1% auto;
     height: 7%;
     text-transform: capitalize;
+    font-family: Poppins;
+    font: caption;
+    letter-spacing: 1.5px;
+    font-size: 1.5rem;
+    @media ${devices.iPad} {
+        height: 5%;
+    }
+
+    @media ${devices.mobileS} {
+        margin: 2% auto 1% 15vw;
+        font-size: 1.3rem;
+    }
 `
 
 export const Heading = styled.div `
     display: grid;
     grid-template-columns: 1fr 1fr;
+    @media ${devices.mobileS} {
+        grid-template-columns: repeat(2, minmax(70vw, 1fr));
+    }
 `
 
 export const Record = styled.div `
@@ -40,6 +61,12 @@ export const Record = styled.div `
     grid-template-columns: 1fr 1fr;
     column-gap: 10%;
     align-items: center;
+    @media ${devices.iPad} {
+        min-height: 6vmax;
+    }
+    @media ${devices.mobileS} {
+        min-height: 5vmax;
+    }
 `
 
 export const Cell = styled.div `
@@ -48,14 +75,19 @@ export const Cell = styled.div `
     display: flex;
     align-items: center;
     justify-content: space-between;
-    border: 1px solid black; 
+    border: 1px solid #A9A9A9; 
     margin-top: 1%;
+    @media ${devices.mobileS} {
+       min-width:70vw;
+       width: auto;
+       font-size: 0.8rem;
+    }
 `
 
 export const Unit = styled.span `
-   padding-right: 5%;
+   padding: 0 5% 0 5%;
 `
 
 export const Value = styled.span `
-   padding-left: 8%;
+   padding-left: 4%;
 `
